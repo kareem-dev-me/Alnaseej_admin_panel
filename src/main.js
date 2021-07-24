030bd1dee7bf7db14150f0f97eb1f0d6544a7efe
Kareem basuony <kareembasuonyg@gmail.com>
2021-07-25 01:35:34 +0200

    Edit server IP

--- src/main.js
+++ src/main.js
@@ -1,33 +1,35 @@
-import Vue from 'vue'
-import router from './router'
+import Vue from "vue";
+import router from "./router";
 
-import BootstrapVue from "bootstrap-vue"
-import axios from 'axios'
+import BootstrapVue from "bootstrap-vue";
+import axios from "axios";
 
-import App from './App'
+import App from "./App";
 
-import Default from './Layout/Wrappers/baseLayout.vue';
-import Pages from './Layout/Wrappers/pagesLayout.vue';
+import Default from "./Layout/Wrappers/baseLayout.vue";
+import Pages from "./Layout/Wrappers/pagesLayout.vue";
 
 Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
-Vue.prototype.$http =  axios.create({
-  baseURL: `http://135.181.36.3:8055`,
-  });
-  Vue.prototype.$http.interceptors.response.use((response) => response, (error) => {
-    if(error.response.status >=403){
-
-       localStorage.removeItem('token');
-      router.replace('/')
+Vue.prototype.$http = axios.create({
+    baseURL: `http://66.29.135.86:8055`
+});
+Vue.prototype.$http.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status >= 403) {
+            localStorage.removeItem("token");
+            router.replace("/");
+        }
     }
-  });
-Vue.component('default-layout', Default);
-Vue.component('userpages-layout', Pages);
+);
+Vue.component("default-layout", Default);
+Vue.component("userpages-layout", Pages);
 
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+    el: "#app",
+    router,
+    template: "<App/>",
+    components: { App }
 });
